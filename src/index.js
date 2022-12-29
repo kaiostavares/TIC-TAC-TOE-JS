78dfc207cf1b90f8a5c7126abc47c2424176462a
Kaio Tavares <98710819+kaiostavares@users.noreply.github.com>
2022-12-28 21:14:45 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,119 @@
+const boxes = Array.from(document.getElementsByClassName("box"));
+const clearBtn = document.getElementById("Clear");
+const gameBtn = document.getElementById("NewGame")
+const player1 = document.getElementById("player1")
+const player2 = document.getElementById("player2")
+let pointsP1 = document.getElementById("pointsPlayer1")
+let pointsP2 = document.getElementById("pointsPlayer2")
+const title = document.getElementById("title")
+const root = document.querySelector(":root")
+const X_player = "X";
+const O_player = "O";
+let currentPLayer = X_player;
+const playersMovements = Array(9).fill(false);
+const winnCondintion = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+let playersMovementsCounter = 0
+
+disabledNames(true)
+gameBtn.addEventListener('click',startGame)
+
+function startGame(){
+  title.innerText = "TIC TAC TOE"
+  player1.style.color = "var(--gameLine)"
+  clearBoard()
+  clearPoints()
+  disabledNames(false)
+  player1.focus()
+  gameBtn.innerText = "NewGame"
+  clearBtn.addEventListener("click", clearBoard);
+}
+
+function boardClicked(e) {
+  disabledNames(true)
+  let currentBox = e.target;
+  if (!playersMovements[currentBox.id]) {
+    playersMovements[currentBox.id] = currentPLayer
+    playersMovementsCounter++
+    if(playersMovementsCounter ==9){
+      title.innerText = "DRAW"
+      return
+    }
+    currentBox.innerText = currentPLayer;
+    
+    if(Winner() !== false){
+      addPoints()
+      let winnBoxes = Winner()
+
+      winnBoxes.map(box=>{
+        boxes[box].style.color = "var(--secundaryColor)"
+      })
+      boxes.forEach((box) => {box.removeEventListener("click", boardClicked)})
+      return
+    }
+
+    changePlayers();
+  }
+}
+
+function changePlayers() {
+  if (currentPLayer == X_player) {
+    currentPLayer = O_player;
+    player1.style.color = "var(--gameLine)"
+    player2.style.color = "var(--primaryColor)"
+  } else {
+    currentPLayer = X_player;
+    player1.style.color = "var(--primaryColor)"
+    player2.style.color = "var(--gameLine)"
+  }
+}
+
+function Winner(){
+  for (const item of winnCondintion) {
+    let [a,b,c] = item
+    
+    if(playersMovements[a] && ((playersMovements[a] == playersMovements[b]) &&(playersMovements[a] == playersMovements[c]))){
+      return [a,b,c]
+    }
+  }
+
+  return false
+}
+
+function addPoints(){
+  if(currentPLayer == X_player){
+    title.innerText = `${player1.value} WON`
+    pointsP1.innerText = `${parseFloat(pointsP1.innerText) +1 }`
+  } else {
+    title.innerText = `${player2.value} WON`
+    pointsP2.innerText = `${parseFloat(pointsP2.innerText) +1 }`
+  }
+}
+
+function clearBoard() {
+  playersMovements.fill(null);
+  title.innerText = "TIC TAC TOE"
+  boxes.forEach((box) => {
+    box.innerText = ""
+    box.style.color = "var(--primaryColor)"
+    box.addEventListener("click",boardClicked)
+  });
+}
+
+function clearPoints(){
+  pointsP1.innerText = 0
+  pointsP2.innerText = 0
+}
+
+function disabledNames(true_false){
+  player1.disabled = true_false
+  player2.disabled = true_false
+}
